933821c4c2996de6e36d2c66a03f342d7853737b
rihan010 <ibrhimrihan000@gmail.comm>
2023-05-03 16:30:12 +0600

    meaning comment and data added

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const express = require('express')
 const cors = require('cors')
-const app = express()
-const port = 5000
+const app = express();
+const port = process.env.PORT || 5000
 const ChefData =require('./data/ChefData.json');
-
-
 app.use(cors());
+
+
+// Chef Data for landing page 
 app.get('/', (req, res) => {
   res.send(ChefData)
 })
+
+// Chef Details data for view details page
+
 app.get('/chefDetails', (req, res) => {
   res.send(ChefData)
 })
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(` ${port}`)
 })
