e2ec57b97f0773ae152045bef621969e3cabcb31
rihan010 <ibrhimrihan000@gmail.comm>
2023-05-03 16:14:25 +0600

    chefdata added

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = 5000
+const ChefData =require('./data/ChefData.json');
 
 
 app.use(cors());
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(ChefData)
 })
-app.get('/ChefData', (req, res) => {
-    res.send(ChefData)
-});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
