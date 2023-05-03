52817f82d38f3768a7bfcb1c95ac5c18d3f0d923
rihan010 <ibrhimrihan000@gmail.comm>
2023-05-03 16:11:18 +0600

    cors added

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 3000
+const port = 5000
 
-const ChefData =require('./data/ChefData.json');
 
 app.use(cors());
 app.get('/', (req, res) => {
